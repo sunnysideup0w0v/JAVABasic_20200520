a0ccbf442eb3c69f077fc06d1f8b8319a441c75a
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-20 16:38:55 +0900

    dataBinding setting

--- app/src/main/java/com/example/javabasic_20200520/MainActivity.java
+++ app/src/main/java/com/example/javabasic_20200520/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.javabasic_20200520;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.javabasic_20200520.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText contentEdt;   // 참조형 변수, 클래스의 멤버변수, 기본값. null
-    Button okBtn;
-    TextView contentTxt;
+    ActivityMainBinding binding;
+
+//    EditText contentEdt;   // 참조형 변수, 클래스의 멤버변수, 기본값. null
+//    Button okBtn;
+//    TextView contentTxt;
 
 
 
@@ -20,9 +26,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        contentEdt = findViewById(R.id.contentEdt);
-        okBtn = findViewById(R.id.okBtn);
-        contentTxt = findViewById(R.id.contentTxt);
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
