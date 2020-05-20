81e311ede66c06a2ddf44334899fc5688689e87e
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-20 16:10:06 +0900

    xml의 뷰들을 java에 연결해주기  - id부여, findViewById로 멤버변수에 연결

--- app/src/main/java/com/example/javabasic_20200520/MainActivity.java
+++ app/src/main/java/com/example/javabasic_20200520/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.javabasic_20200520;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText contentEdt;   // 참조형 변수, 클래스의 멤버변수, 기본값. null
+    Button okBtn;
+    TextView contentTxt;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contentEdt = findViewById(R.id.contentEdt);
+        okBtn = findViewById(R.id.okBtn);
+        contentTxt = findViewById(R.id.contentTxt);
+
     }
 }
